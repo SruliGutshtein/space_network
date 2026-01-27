8dbfd323358b8bd979d7fdd9f54943c9fed81058
sruli <Y0527190457@GMAIL.COM>
2026-01-27 10:49:20 +0200

    יצירת פונקציה שמוודאת שליחת הודעה אחרי טיפול בשגיאות

--- space_network.py
+++ space_network.py
@@ -1,12 +1,32 @@
-from space_network_lib import SpaceEntity, SpaceNetwork, Packet
+import time
+
+from space_network_lib import SpaceEntity, SpaceNetwork, Packet, TemporalInterferenceError, DataCorruptedError
 
 class Satellite(SpaceEntity):
     def receive_signal(self, packet):
         """מדפיסה את הפקטה עם שם הלווין"""
         print(f"[{self.name}] Received: {packet}")
 
-space_network = SpaceNetwork(1)
+def attempt_transmission(packet):
+    success = False
+    while not success:
+        try:
+            network.send(packet)
+            success = True
+        except TemporalInterferenceError:
+            print("Temporary interference, please retry")
+            time.sleep(2)
+        except DataCorruptedError:
+            print("Data corrupted during transmission")
+
+
+
+
+
+
+
+network = SpaceNetwork(2)
 sat1 = Satellite("Sat1", 100)
 sat2 = Satellite("Sat2", 200)
 my_packet = Packet("היי, אחת שתיים, שומע שומע?", sat1, sat2)
-space_network.send(my_packet)
+network.send(my_packet)
